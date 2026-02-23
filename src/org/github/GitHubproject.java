4f4951aeb3a6a3858da7c1907a38612586b5627f
Mohank10 <akmohan1410@gmail.com>
2026-02-23 10:47:13 +0530

    Kumar work done

--- src/org/github/GitHubproject.java
+++ src/org/github/GitHubproject.java
@@ -3,7 +3,13 @@ package org.github;
 public class GitHubproject {
 	public static void main(String[] args) {
 		
-		
+		int a=450;
+		if(a<500) {
+			System.out.println(a);
+		}
+		else {
+			System.out.println("Not satisfied");
+		}
 		System.out.println("github done");
 	}
 	
